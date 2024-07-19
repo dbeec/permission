9bce94fc05a86b769befea7c8501e4f0e85079fc
dbee <dev.beecodes@gmail.com>
2024-07-19 11:44:03 -0500

    updated

--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -27,7 +27,7 @@ export default function Sidebar() {
   useEffect(() => {
     const fetchRoutes = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/v1/routes");
+        const response = await axios.get("http://localhost:4000/v1/routes");
         setRoutes(response.data);
       } catch (error) {
         console.log("Error fetching routes...");
